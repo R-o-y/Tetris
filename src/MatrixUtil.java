ef51095825421792d3e958cb661deca28c718dec
R-o-y <royal845426589@gmail.com>
2017-03-27 01:36:54 +0800

    add MatrixUtil.java

---
+++ src/MatrixUtil.java
@@ -0,0 +1,10 @@
+/**
+ * This is the utility class 
+ * that implements matrix operations used in Linear Square Policy Iteration
+ * All the operations are implemented as a static method
+ * @author Roy
+ *
+ */
+public class MatrixUtil {
+
+}
